3d3892ee0f0926dd2a07ed7f3a8bb79495d72029
opemily <opemily@gmail.com>
2015-06-01 23:04:11 -0400

    added media query and js for mobile nav menu

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+
+	////////// Mobile Menu ///////////
+
+	$('a.menu-icon').click(function() {
+		$('#menu-links').slideToggle();
+	});
+
+	//fix hidden links on window resize
+
+	$(window).resize(function() {
+		if ($(window).width() > 700) {
+			$('#menu-links').removeAttr('style');
+		 }
+		});
+});
